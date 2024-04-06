9ac7e7cbfbf86cd0bf502fe2b6efe63ff144b7a9
Andy Bruce <acbruce@ucsc.edu>
2024-04-06 15:05:53 -0700

    linter

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import js from "@eslint/js";
+import globals from "globals";
+export default [
+    js.configs.recommended,
+    {
+        rules:{
+	    "no-unused-vars": [
+		"error",
+		{ varsIgnorePattern: "^_" },
+	    ]
+	},
+	languageOptions: {
+	    globals: {
+                ...globals.browser,
+            }
+	}
+    }
+];
